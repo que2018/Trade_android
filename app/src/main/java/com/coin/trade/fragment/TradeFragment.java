477dbca6b376d5cbca40dba3d41cf6e667603fdb
que2018 <goodiswill2016@gmail.com>
2018-05-08 18:35:44 -0700

    Initial commit

---
+++ app/src/main/java/com/coin/trade/fragment/TradeFragment.java
@@ -0,0 +1,25 @@
+package com.coin.trade.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.coin.trade.R;
+
+
+public class TradeFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_trade, null);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/coin/trade/main/MainActivity.java
@@ -0,0 +1,107 @@
+package com.coin.trade.main;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import com.coin.trade.fragment.HomeFragment;
+import com.coin.trade.fragment.QuotationFragment;
+import com.coin.trade.fragment.TradeFragment;
+import com.coin.trade.fragment.AccountFragment;
+import com.coin.trade.helper.BottomNavigationViewHelper;
+import com.coin.trade.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Fragment[] fragments;
+    private int currentFragment = 0;
+    
+    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+       @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.nav_home:
+                    showFragment(0);
+                    return true;
+
+                case R.id.nav_quotation:
+                    showFragment(1);
+                    return true;
+
+                case R.id.nav_trade:
+                    showFragment(2);
+                    return true;
+					
+				case R.id.nav_account:
+                    showFragment(3);
+                    return true;
+            }
+
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+		//fragments
+        HomeFragment homeFragment = new HomeFragment();
+       
+        QuotationFragment quotationFragment = new QuotationFragment();
+   
+        TradeFragment tradeFragment = new TradeFragment();
+		
+		AccountFragment accountFragment = new AccountFragment();
+       
+        fragments = new Fragment[] { homeFragment, quotationFragment, tradeFragment, accountFragment };
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_container, homeFragment)
+                .add(R.id.fragment_container, quotationFragment)
+				.add(R.id.fragment_container, tradeFragment)
+                .add(R.id.fragment_container, accountFragment)
+                .hide(quotationFragment)
+				.hide(tradeFragment)
+                .hide(accountFragment)
+                .show(homeFragment).commit();
+
+		//navigation
+        BottomNavigationView navigation = (BottomNavigationView)findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+		navigation.setItemIconTintList(null);
+    
+        BottomNavigationViewHelper.removeShiftMode(navigation);
+    }
+
+    protected void showFragment(int index)
+    {
+        if (index != currentFragment)
+        {
+            currentFragment = index;
+        }
+    
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+    
+        for (int i = 0; i < fragments.length; i++)
+        {
+            fragmentTransaction.hide(fragments[i]);
+        }
+    
+        if (!fragments[index].isAdded())
+        {
+            fragmentTransaction.add(R.id.fragment_container, fragments[index]);
+        }
+    
+        fragmentTransaction.show(fragments[index]).commit();
+    }
+}
