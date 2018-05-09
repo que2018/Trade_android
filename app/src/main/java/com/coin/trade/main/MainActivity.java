b86ad4e50a8243eedd3acdee7febfcb152e778bd
que2018 <goodiswill2016@gmail.com>
2018-05-08 18:37:09 -0700

    F

--- app/src/main/java/com/coin/trade/main/MainActivity.java
+++ app/src/main/java/com/coin/trade/main/MainActivity.java
@@ -19,7 +19,7 @@ import com.coin.trade.R;
 public class MainActivity extends AppCompatActivity {
 
     private Fragment[] fragments;
-    private int currentFragment = 0;
+    private int currentFragment = 1;
     
     private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
